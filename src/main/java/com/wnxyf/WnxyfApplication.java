a3968b16439bab067388759de668e22fea305ac0
huanw <huanw@mondora.cn>
2018-06-22 14:14:39 +0800

    初始化

---
+++ src/main/java/com/wnxyf/WnxyfApplication.java
@@ -0,0 +1,12 @@
+package com.wnxyf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnxyfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WnxyfApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wnxyf/WnxyfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wnxyf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WnxyfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
